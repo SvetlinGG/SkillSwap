40718342b48c3e749349a477e9f2befe609fc3c2
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-13 22:01:09 +0200

    login component simple UI

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home/home.component';
+import { LoginComponent } from './features/auth/login/login.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    
+    {path: 'login', component: LoginComponent},
+
+
+    {path: '**', redirectTo: ''}
 ];
---
+++ client/src/app/features/auth/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  email = '';
+  password = '';
+  login(){
+    console.log(this.email, this.password);
+    
+  }
+
+}
