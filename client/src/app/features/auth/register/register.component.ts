6a4e0fe18b81e00a5bd796b82daabadba7369d8f
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-13 22:06:08 +0200

    routing and simple UI of register component

--- client/src/app/features/auth/register/register.component.ts
+++ client/src/app/features/auth/register/register.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  email = '';
+  password = '';
+
+  register(){
+    console.log(this.email, this.password);
+    
+  }
 
 }
