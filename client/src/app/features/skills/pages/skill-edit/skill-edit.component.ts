d8b47b7b04ccc5e00d7e0730a70b0f8d418cb6bf
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-17 23:47:37 +0200

    create skill-edit component

---
+++ client/src/app/features/skills/pages/skill-edit/skill-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-edit',
+  imports: [],
+  templateUrl: './skill-edit.component.html',
+  styleUrl: './skill-edit.component.css'
+})
+export class SkillEditComponent {
+
+}
