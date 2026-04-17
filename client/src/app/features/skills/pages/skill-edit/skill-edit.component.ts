eb5d6d7a58fcbc4c1fc8ea9976fed97091b0b821
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-17 23:54:53 +0200

    skill-edit html

--- client/src/app/features/skills/pages/skill-edit/skill-edit.component.ts
+++ client/src/app/features/skills/pages/skill-edit/skill-edit.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Skill } from '../../models/skill.model';
 
 @Component({
   selector: 'app-skill-edit',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './skill-edit.component.html',
   styleUrl: './skill-edit.component.css'
 })
-export class SkillEditComponent {
+export class SkillEditComponent implements OnInit {
+
+  skill: Skill = {
+    title: '',
+    description: '',
+    category: '',
+    level: 'Beginner',
+    owner: ''
+  };
+
+  isLoading = true;
+
+
+  ngOnInit(): void {
+    
+  }
+
+  submit(){
+    
+  }
 
 }
