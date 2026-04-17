ed9c9dc756fc78aed28e5af6dfbd18c719a1f4c7
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-17 23:20:25 +0200

    backend skills - GET by id

--- server/src/routes/skillRoutes.js
+++ server/src/routes/skillRoutes.js
@@ -15,9 +15,16 @@ router.get('/', async (req, res) => {
 
 
 router.get('/:id', async (req, res) => {
-    const skill = await Skill.findById(req.params.id);
-    if (!skill) return res.status(404).json({ message: 'Skill not found' });
-    res.json(skill);
+
+    try {
+        const skill = await Skill.findById(req.params.id);
+
+        if (!skill) return res.status(404).json({ message: 'Skill not found' });
+        res.json(skill);
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to fetch skill details'});
+    }
+    
 });
 
 
