05cd3a237cddeb2f621545b16aa6c4b5cf6d4275
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-18 01:10:48 +0200

    setup skills.routes.ts

--- client/src/app/features/skills/skills.routes.ts
+++ client/src/app/features/skills/skills.routes.ts
@@ -14,6 +14,12 @@ export const skillsRoutes: Routes = [
             .then( c => c.SkillCreateComponent)
     },
     {
+        path: 'edit/:id',
+        loadComponent: () =>
+          import('./pages/skill-edit/skill-edit.component')
+            .then(c => c.SkillEditComponent)
+    },
+    {
         path: ':id',
         loadComponent: () => 
             import('./pages/skill-details/skill-details.component')
