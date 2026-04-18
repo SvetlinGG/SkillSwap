95132b743999c5f892f59394e266c4d8c7f7fa11
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-18 23:41:47 +0200

    use signal in skill details component

--- client/src/app/features/skills/pages/skill-details/skill-details.component.ts
+++ client/src/app/features/skills/pages/skill-details/skill-details.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { SkillsService } from '../../services/skills.service';
 import { Skill } from '../../models/skill.model';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-skill-details',
@@ -11,15 +12,28 @@ import { Skill } from '../../models/skill.model';
   templateUrl: './skill-details.component.html',
   styleUrl: './skill-details.component.css'
 })
-export class SkillDetailsComponent implements OnInit {
+export class SkillDetailsComponent implements OnInit  {
 
-  skill!: Skill;
-  isLoading = true;
+  // private route = inject(ActivatedRoute);
+  // private router = inject(Router);
+  // private skillsService = inject(SkillsService);
+  // private authService = inject(AuthService);
+
+  skill = signal<Skill | null>(null);
+  isLoading = signal(true);
+
+  isOwner = computed(() => {
+    const currentUserId = this.authService.getCurrentUserId();
+    const currentSkill = this.skill();
+
+    return !!currentUserId && !!currentSkill && currentSkill.owner === currentUserId;
+  });
 
   constructor( 
     private route: ActivatedRoute, 
     private router: Router,
-    private skillsService: SkillsService
+    private skillsService: SkillsService,
+    private authService: AuthService
   ){}
 
   ngOnInit(): void {
@@ -32,27 +46,27 @@ export class SkillDetailsComponent implements OnInit {
 
     this.skillsService.getSkillById(id).subscribe({
       next: (data) =>{
-      this.skill = data;
-      this.isLoading = false;
+      this.skill.set(data);
+      this.isLoading.set(false);
       },
       error: () => this.router.navigate(['/skills'])
     });
   }
 
   onDelete(): void{
-    const id = this.route.snapshot.paramMap.get('id');
+    const currentSkill = this.skill();
 
-    if(!id){
+    if(!currentSkill?._id){
       return
     }
 
-    const hasConfirmed = confirm(`Delete "${this.skill.title}"?`);
+    const hasConfirmed = confirm(`Delete "${currentSkill.title}"?`);
 
     if (!hasConfirmed){
       return;
     }
 
-    this.skillsService.deleteSkill(id).subscribe({
+    this.skillsService.deleteSkill(currentSkill._id).subscribe({
       next: () => this.router.navigate(['/skills']),
       error: (err) => alert(err.error?.message || 'Failed to delete skill')
       
