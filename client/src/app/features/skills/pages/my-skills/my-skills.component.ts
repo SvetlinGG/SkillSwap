28a6aad495e49b0f900e91c66f38303f6cddd36f
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-19 03:36:44 +0200

    create my-skill component

---
+++ client/src/app/features/skills/pages/my-skills/my-skills.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SkillsService } from '../../services/skills.service';
+import { Skill } from '../../models/skill.model';
+
+@Component({
+  selector: 'app-my-skills',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './my-skills.component.html',
+  styleUrl: './my-skills.component.css'
+})
+export class MySkillsComponent implements OnInit {
+
+  constructor(private skillsService: SkillsService){}
+
+  mySkills = signal<Skill[]>([]);
+  isLoading = signal(true);
+  errorMessage = signal('');
+
+  ngOnInit(): void {
+    this.skillsService.getMySkills().subscribe({
+      next: (skills) => {
+        this.mySkills.set(skills);
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        this.errorMessage.set(err.error?.message || 'Failed to load your skills');
+        this.isLoading.set(false);
+      }
+    });
+  }
+}
