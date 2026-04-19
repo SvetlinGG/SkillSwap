312c09c4ea9c2eb235639ea76e4e5154eb3e0b2d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-20 00:51:09 +0200

    setup dynamc header in header component

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../features/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  isLoggedIn = this.authService.isLoggedIn;
+  userEmail = computed(() => this.authService.user()?.email || '');
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 
 }
