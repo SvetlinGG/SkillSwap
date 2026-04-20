f670c053ca239c6613e3534cfbe4cfcedfb9bee3
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-21 00:47:55 +0200

    change in cors()

--- server/src/app.js
+++ server/src/app.js
@@ -5,11 +5,23 @@ import skillRoutes from './routes/skillRoutes.js';
 
 export const app = express();
 
-app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/skills', skillRoutes);
+
 app.get('/', (req, res) => {
-    res.send('SkillSwap API is running')
-})
+  console.log('HIT /');
+  res.send('ROOT OK');
+});
+
+app.get('/api/test', (req, res) => {
+  console.log('HIT /api/test');
+  res.json({ message: 'API works' });
+});
