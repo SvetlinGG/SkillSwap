f6e188467442e37ab89b8bd35bc14579201f4850
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-20 22:21:25 +0200

    create request collection with mongoose

---
+++ server/src/models/Request.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const requestSchema = new mongoose.Schema(
+    {
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+          },
+          receiver: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+          },
+          skillOffered: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Skill',
+            required: true
+          },
+          message: {
+            type: String,
+            maxlength: 200,
+            default: ''
+          },
+          status: {
+            type: String,
+            enum: ['pending', 'accepted', 'rejected'],
+            default: 'pending'
+          }
+        },
+        { timestamps: true }
+      
+)
+
+export const Request = mongoose.model('Request', userSchema);
