6e4395310082296c4cd9e663ec543dc4660a4d0b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-22 23:26:19 +0200

    catchError in skill service file

--- client/src/app/features/skills/services/skills.service.ts
+++ client/src/app/features/skills/services/skills.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Skill } from '../models/skill.model';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 
 
 
@@ -13,26 +13,61 @@ export class SkillsService {
   constructor(private http: HttpClient) { }
 
   getSkills(): Observable<Skill[]>{
-    return this.http.get<Skill[]>(this.apiUrl);
+    return this.http.get<Skill[]>(this.apiUrl).pipe(
+      map((skills) => skills.sort((a, b) =>
+      (b.createdAt || '').localeCompare(a.createdAt || '')
+    )),
+    catchError((error) => {
+      console.error('GET SKILLS ERROR:', error);
+      return throwError(() => new Error('Failed to load skills.'))
+    })
+    );
   }
 
   getMySkills(): Observable<Skill[]> {
-    return this.http.get<Skill[]>(`${this.apiUrl}/my-skills`)
+    return this.http.get<Skill[]>(`${this.apiUrl}/my-skills`).pipe(
+      map((skills) => skills.filter(skill => !!skill._id)),
+      catchError((error) => {
+        console.error('GET MY SKILLS ERROR:', error);
+        return throwError(() => new Error('Failed to load your skills.'))
+      })
+    );
   }
 
   getSkillById(id: string): Observable<Skill> {
-    return this.http.get<Skill>(`${this.apiUrl}/${id}`);
+    return this.http.get<Skill>(`${this.apiUrl}/${id}`).pipe(
+      catchError((error) => {
+        console.error('GET SKILL DETAILS ERROR:', error);
+        return throwError(() => new Error('Failed to load skill details.'))
+      })
+    );
   }
 
   createSkill(skillData: Omit<Skill, '_id' | 'owner' | 'createdAt' | 'updatedAt'>): Observable<Skill>{
-    return this.http.post<Skill>(this.apiUrl, skillData)
+    return this.http.post<Skill>(this.apiUrl, skillData).pipe(
+      catchError((error) => {
+        console.error('CREATE SKILL ERROR:', error);
+        return throwError(() => new Error(error?.error?.message || 'Failed to create skill'))
+      })
+    );
   }
 
   updateSkill(id: string, skillData: Partial<Skill>): Observable<Skill>{
-    return this.http.put<Skill>(`${this.apiUrl}/${id}`, skillData);
+    return this.http.put<Skill>(`${this.apiUrl}/${id}`, skillData).pipe(
+      catchError((error) => {
+        console.error('UPDATE SKILL ERROR:', error);
+        return throwError(() => new Error(error?.error?.message || 'Failed to update skill.'));
+      })
+    );
   }
 
   deleteSkill(id: string): Observable<{message: string}>{
-    return this.http.delete<{message: string}>(`${this.apiUrl}/${id}`)
+    return this.http.delete<{message: string}>(`${this.apiUrl}/${id}`).pipe(
+      catchError((error) => {
+        console.error('DELETE SKILL ERROR:', error);
+        return throwError(() => new Error(error?.error?.message || 'Failed to delete skill.'));
+      })
+      
+    );
   }
 }
