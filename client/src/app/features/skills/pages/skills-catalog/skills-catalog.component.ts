1e6725f513c5b370a869893ee2aa12a77e60af66
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-23 00:44:48 +0200

    updates pipes in components

--- client/src/app/features/skills/pages/skills-catalog/skills-catalog.component.ts
+++ client/src/app/features/skills/pages/skills-catalog/skills-catalog.component.ts
@@ -1,19 +1,20 @@
-import { CommonModule } from '@angular/common';
+
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SkillsService } from '../../services/skills.service';
 import { Skill } from '../../models/skill.model';
+import { TruncatePipe } from '../../../../shared/pipes/truncate.pipe';
 
 @Component({
   selector: 'app-skills-catalog',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [RouterLink, TruncatePipe],
   templateUrl: './skills-catalog.component.html',
   styleUrl: './skills-catalog.component.css'
 })
 export class SkillsCatalogComponent implements OnInit {
 
-  skills: Skill[] = [];
+  skills: any[] = [];
 
   constructor(private skillsService: SkillsService){}
 
