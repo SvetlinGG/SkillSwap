198748533d5636a9dff44aed80dd258a602ba6ae
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-27 22:21:03 +0200

    add in skill service

--- client/src/app/features/skills/services/skills.service.ts
+++ client/src/app/features/skills/services/skills.service.ts
@@ -49,6 +49,15 @@ export class SkillsService {
     );
   }
 
+  getLikedSkills(){
+    return this.http.get<Skill>(`${this.apiUrl}/liked`).pipe(
+      catchError((error) => {
+        console.error('GET LIKED SKILLS ERROR:', error);
+        return throwError(() => new Error(error?.error?.message || 'Failed to load liked skills.'))
+      })
+    );
+  }
+
   addComment( id: string, text: string){
     return this.http.post<Skill>(`${this.apiUrl}/${id}/comments`, { text }).pipe(
       catchError((error) => {
