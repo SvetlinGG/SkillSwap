a77ecb56dd59e69f6163db97178740968498a871
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-28 02:18:59 +0200

    test for manual deploy

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -6,6 +6,9 @@ dotenv.config();
 
 export const connectDB = async () => {
     try {
+        if (!process.env.MONGO_URI){
+            throw new Error('MONGO_URI is missing')
+        }
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
         
